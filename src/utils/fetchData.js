529c7c3241f69193b3ff68f3c718daade7f77bdd
Bakr Ibrahimi <bakribra5@gmail.com>
2023-12-01 00:32:08 +0100

    first commit:all project

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'da50e867dfmsh62c7b3341bd1710p101c9fjsna24de86db22e',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '15b423e5c1msh4ed27c6139bfa94p1ee7d4jsnfda6bb6c2fa6',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
